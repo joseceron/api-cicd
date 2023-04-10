a77a96f4af955c543462aed84ed0f46b3b8dc975
Jose Ceron <joseluisceron13@gmail.com>
2023-04-10 18:59:27 -0500

    feat: set users api get method to respond with a generic object

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const user = {id: 1, name: 'Jose Luis'}
+  res.status(200).json(user)
 });
 
 module.exports = router;
